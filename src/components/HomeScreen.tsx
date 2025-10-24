c6fa9f4b596a20200eb8178b2746a0814daa4382
juice <kanonao24203124@gmail.com>
2025-10-25 00:14:13 +0900

    リファクタリング

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -391,7 +391,7 @@ export const HomeScreen = () => {
                     onClick={() => setIsDropdownOpen(prev => !prev)}
                     className="w-full flex justify-start items-center text-gray-500 dark:text-gray-400 bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5 mr-2"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-7 h-7 mr-2"><circle cx="11" cy="11" r="8" /><path d="m16.5 16.5 4.5 4.5" /></svg>
                     {selectedBuilding && selectedRoom
                         ? `目的地: ${selectedBuilding} ${selectedRoom}`
                         : selectedBuilding
